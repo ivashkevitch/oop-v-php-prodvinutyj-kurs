d93157327d462ddb06e4a4127f1eafef8699a567
Artyom Ivashkevich <artyom.ivashkevich@gmail.com>
2021-01-31 19:35:10 +0700

    https://webshake.ru/oop-v-php-prodvinutyj-kurs/pattern-singleton-v-php

--- myproject.loc/src/MyProject/Models/ActiveRecordEntity.php
+++ myproject.loc/src/MyProject/Models/ActiveRecordEntity.php
@@ -33,7 +33,7 @@ abstract class ActiveRecordEntity
      */
     public static function findAll(): array
     {
-        $db = new Db();
+        $db = $db = Db::getInstance();
         return $db->query('SELECT * FROM `' . static::getTableName() . '`;', [], static::class);
     }
 
@@ -43,7 +43,7 @@ abstract class ActiveRecordEntity
      */
     public static function getById(int $id): ?self
     {
-        $db = new Db();
+        $db = $db = Db::getInstance();
         $entities = $db->query(
             'SELECT * FROM `' . static::getTableName() . '` WHERE id=:id;',
             [':id' => $id],
--- myproject.loc/src/MyProject/Services/Db.php
+++ myproject.loc/src/MyProject/Services/Db.php
@@ -4,10 +4,12 @@ namespace MyProject\Services;
 
 class Db
 {
+    private static $instance;
+
     /** @var \PDO */
     private $pdo;
 
-    public function __construct()
+    private function __construct()
     {
         $dbOptions = (require __DIR__ . '/../../settings.php')['db'];
 
@@ -29,5 +31,14 @@ class Db
         }
 
         return $sth->fetchAll(\PDO::FETCH_CLASS, $className);
+    }
+
+    public static function getInstance(): self
+    {
+        if (self::$instance === null) {
+            self::$instance = new self();
+        }
+
+        return self::$instance;
     }
 }
